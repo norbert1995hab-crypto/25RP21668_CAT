68ef294831876fae6c23d0444b1b6ae738d84f41
Norbert HABIYEZE <norbert1995hab@gmail.com>
2025-12-09 20:16:21 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Import Express
+const express = require('express');
+const app = express();
+
+// Server port
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Home route
+app.get('/', (req, res) => {
+    res.send('Welcome to the WebApp Project!');
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
